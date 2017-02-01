99fbc2dcda8551b584260a25c1bd308b61e91aa1
Eirik Kvarstein <eirik@detec.no>
2017-02-01 14:47:48 +0100

    initial commit

---
+++ AuctionHouse/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // netsh http add urlacl url=http://+:88/ user=detec\eirik
+            var url = "http://+:3333";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Running on {0}", url);
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
