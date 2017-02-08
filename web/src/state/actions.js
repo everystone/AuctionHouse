6b39ff07d04e5645e0c7d5c7fff2ba37e2ea51e3
Eirik Kvarstein <eirik.kvarstein@gmail.com>
2017-02-08 13:29:58 +0100

    Moved vuex actions into seperate file.

---
+++ web/src/state/actions.js
@@ -0,0 +1,61 @@
+import Api from '../api'
+import Constants from '../Constants'
+
+export default {
+  fetch ({commit}, context) {
+    // commit('FETCH_ITEMS', context)
+    Api.get(Constants.LIST_MATERIAL).then(list => {
+      commit('RETRIEVED_ITEMS', list)
+    }, error => {
+      context.error = error
+    })
+  },
+  saveEdit ({commit, state}, context) {
+    let item = {
+      Id: state.edit.id,
+      Name: state.edit.name,
+      Price: state.edit.price,
+      Labor: state.edit.labor,
+      Produce: state.edit.produce,
+      craftingRecipe: state.edit.craftingRecipe,
+      History: state.edit.history
+    }
+    return new Promise((resolve, reject) => {
+      Api.post(context, Constants.SAVE_ITEM, item).then(items => {
+        commit('UPDATE_ITEMS', items)
+        resolve(items)
+      }, error => {
+        reject(error)
+      })
+    })
+  },
+  quickSave({commit, state}, data) {
+    return new Promise((resolve, reject) => {
+      Api.post(data.context, Constants.SAVE_ITEM, data.item).then(items => {
+        commit('UPDATE_ITEMS', items)
+        resolve(items)
+      }, error => {
+        reject(error)
+      })
+    })
+  },
+  load ({commit}, item) {
+    commit('LOAD', item)
+  },
+  login ({commit}, data) {
+    let context = data.context
+    let creds = data.creds
+    console.log('creds: ' + creds)
+    console.log(Constants.LOGIN_URL)
+    Api.post(Constants.LOGIN_URL, creds).then(result => {
+      Api.user.authenticated = true
+      localStorage.setItem('token', result) // eslint-disable-line
+      context.$router.push('home')
+    }, error => {
+      context.error = error
+    })
+  },
+  filter ({commit, state}, str) {
+    commit('FILTER', str)
+  }
+}
--- web/src/state/store.js
+++ web/src/state/store.js
@@ -1,7 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import Api from '../api'
-import Constants from '../Constants'
+import Actions from './actions'
 
 Vue.use(Vuex)
 export default new Vuex.Store({
@@ -36,62 +35,5 @@ export default new Vuex.Store({
       state.filtered = state.items.filter(m => m.name.toLowerCase().indexOf(str.toLowerCase()) >= 0)
     }
   },
-  actions: {
-    fetch ({commit}, context) {
-      // commit('FETCH_ITEMS', context)
-      Api.get(Constants.LIST_MATERIAL).then(list => {
-        commit('RETRIEVED_ITEMS', list)
-      }, error => {
-        context.error = error
-      })
-    },
-    saveEdit ({commit, state}, context) {
-      let item = {
-        Id: state.edit.id,
-        Name: state.edit.name,
-        Price: state.edit.price,
-        Labor: state.edit.labor,
-        Produce: state.edit.produce,
-        craftingRecipe: state.edit.craftingRecipe,
-        History: state.edit.history
-      }
-      return new Promise((resolve, reject) => {
-        Api.post(context, Constants.SAVE_ITEM, item).then(items => {
-          commit('UPDATE_ITEMS', items)
-          resolve(items)
-        }, error => {
-          reject(error)
-        })
-      })
-    },
-    quickSave({commit, state}, data) {
-      return new Promise((resolve, reject) => {
-        Api.post(data.context, Constants.SAVE_ITEM, data.item).then(items => {
-          commit('UPDATE_ITEMS', items)
-          resolve(items)
-        }, error => {
-          reject(error)
-        })
-      })
-    },
-    load ({commit}, item) {
-      commit('LOAD', item)
-    },
-    login ({commit}, data) {
-      let context = data.context
-      let creds = data.creds
-      console.log('creds: ' + creds)
-      console.log(Constants.LOGIN_URL)
-      Api.post(Constants.LOGIN_URL, creds).then(result => {
-        Api.user.authenticated = true
-        localStorage.setItem('token', result) // eslint-disable-line
-        context.$router.push('home')
-      }, error => {
-        context.error = error
-      })
-    },
-    filter ({commit, state}, str) {
-      commit('FILTER', str)
-    }
-  }
+  actions: Actions
 })
